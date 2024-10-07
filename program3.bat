0832df2c7d5b0780003c951c5226754479312cad
scudiriaf <166481656+scudiriaf@users.noreply.github.com>
2024-10-07 21:33:58 +0300

    Add files via upload

---
+++ program3.bat
@@ -0,0 +1,22 @@
+echo off
+md Aboelkomsan
+cd Aboelkomsan
+md Mohamed
+md Aboelkomsan
+pause
+cd Mohamed
+echo > 15.txt
+cd..
+cd Aboelkomsan
+echo > 14022003.txt
+cd..
+cd..
+pause
+del Aboelkomsan /S /Q /F
+pause
+cd Aboelkomsan
+rd Aboelkomsan
+rd Mohamed
+cd..
+rd Aboelkomsan
+pause
